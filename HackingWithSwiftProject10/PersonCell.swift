6bb8c9dc61140ede09c8bb209460641dc7d2fd41
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-10-31 07:43:51 -0700

    Initiated project 10

---
+++ HackingWithSwiftProject10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  HackingWithSwiftProject10
+//
+//  Created by user208023 on 10/31/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
